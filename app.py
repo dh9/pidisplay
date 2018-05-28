979e69033d9f85989e0261688aa8ab0a9ffe8909
Dan Harris <danharris@dans-air.home>
2018-05-28 09:24:54 +0100

    add text api

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    display.display('Hello Flask from ubuntu!')
-    return 'Flask Dockerized'
+    display.display('Hello Flask!')
+    return 'Hello Flask!'
+
+@app.route('/api/<text>')
+def api_text(text):
+    display.display(text)
+    return text
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
