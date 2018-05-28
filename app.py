ee97000ab144726fd047778b3f4e27053edfefca
Dan Harris <danharris@dans-air.home>
2018-05-28 09:31:25 +0100

    add ability to send full string

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import display
 
-from flask import Flask
+from flask import Flask, json
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     display.display('Hello Flask!')
     return 'Hello Flask!'
+
+@app.route('/message', methods = ['POST'])
+def api_message():
+    if request.headers['Content-Type'] == 'text/plain':
+        display.display(request.data)
+        return "Text Message: " + request.data
+
+    elif request.headers['Content-Type'] == 'application/json':
+        display.display(json.dumps(request.json))
+        return "JSON Message: " + json.dumps(request.json)
 
 @app.route('/api/<text>')
 def api_text(text):
