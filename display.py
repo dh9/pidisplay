447a0e9174e93299a8bf540c5ed09d6b9640037f
Dan Harris <danharris@dans-air.home>
2018-05-28 09:15:55 +0100

    fix typo

--- display.py
+++ display.py
@@ -6,7 +6,7 @@ from luma.core.render import canvas
 from luma.core.legacy import show_message
 from luma.core.legacy.font import proportional, LCD_FONT
 
-def display(message)
+def display(message):
     serial = spi(port=0, device=0, gpio=noop())
     device = max7219(serial, cascaded=4, block_orientation=-90)
 
